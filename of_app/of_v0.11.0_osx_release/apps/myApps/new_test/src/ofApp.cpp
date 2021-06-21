d10b6a125d5ac39e344c066414f3bfdd1536ba8d
Antonio Hofmeister <hof.antonio@gmail.com>
2021-06-22 00:48:05 +0200

    last adjust

--- of_app/of_v0.11.0_osx_release/apps/myApps/new_test/src/ofApp.cpp
+++ of_app/of_v0.11.0_osx_release/apps/myApps/new_test/src/ofApp.cpp
@@ -212,7 +212,7 @@ void ofApp::draw(){
     // draw the scopes
     ofPushMatrix();
         ofSetColor( brightColor );
-    int boxWidth = 327;
+    int boxWidth = 330;
     int boxHeight = 100;
     
     int space = 20;
